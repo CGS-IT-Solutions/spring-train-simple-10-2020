27129938e1e1f4e0599ed4e3c4813de5f3e83020
ext_christian.schaef <christian.schaefer@cgs.at>
2020-10-13 20:43:15 +0200

    # initial version

---
+++ src/main/java/at/cgsit/training/firstexample/SpringTrainSimple012020Application.java
@@ -0,0 +1,13 @@
+package at.cgsit.training.firstexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+public class SpringTrainSimple012020Application {
+
+  public static void mainNoMain(String[] args) {
+    //SpringApplication.run(SpringTrainSimple012020Application.class, args);
+  }
+
+}
