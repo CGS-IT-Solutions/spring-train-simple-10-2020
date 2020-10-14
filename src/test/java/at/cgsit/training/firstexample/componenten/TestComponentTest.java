2d870a468f88bf71ae17e696278480aa29e70d18
ext_christian.schaef <christian.schaefer@cgs.at>
2020-10-14 12:15:52 +0200

    # 1 update 1

--- src/test/java/at/cgsit/training/firstexample/componenten/TestComponentTest.java
+++ src/test/java/at/cgsit/training/firstexample/componenten/TestComponentTest.java
@@ -3,10 +3,7 @@ package at.cgsit.training.firstexample.componenten;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TestComponentTest {
