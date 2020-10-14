c79d04b175c9bb37292aeda519612a7773b153c7
ext_christian.schaef <christian.schaefer@cgs.at>
2020-10-14 13:34:38 +0200

    # initial version

--- src/main/java/at/cgsit/training/firstexample/componenten/SingletonPropertyExample.java
+++ src/main/java/at/cgsit/training/firstexample/componenten/SingletonPropertyExample.java
@@ -1,15 +1,23 @@
 package at.cgsit.training.firstexample.componenten;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("singleton") // change to prototype
 @PropertySource("classpath:myextra.properties")
 public class SingletonPropertyExample {
+
+  Logger logger = LoggerFactory.getLogger(TestComponent.class);
+
+  @Autowired Environment env;
 
   @Value("${myapp.name}")
   private String name;
@@ -18,6 +26,9 @@ public class SingletonPropertyExample {
   private String author;
 
   public String getMessage() {
+
+    String property = env.getProperty("myapp.name");
+    logger.info("value from ENV {}", property);
 
     return String.format("Application %s was created by %s", name, author);
   }
