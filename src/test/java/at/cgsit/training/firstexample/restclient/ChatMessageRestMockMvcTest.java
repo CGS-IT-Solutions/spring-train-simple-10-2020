a636e245d184473e0b87a968ab10ceeffea49ca7
ext_christian.schaef <christian.schaefer@cgs.at>
2020-10-15 13:41:01 +0200

    # Chat message Controller

--- src/test/java/at/cgsit/training/firstexample/restclient/ChatMessageRestMockMvcTest.java
+++ src/test/java/at/cgsit/training/firstexample/restclient/ChatMessageRestMockMvcTest.java
@@ -2,6 +2,9 @@ package at.cgsit.training.firstexample.restclient;
 
 import at.cgsit.training.firstexample.chat.model.ChatMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -32,7 +35,10 @@ public class ChatMessageRestMockMvcTest {
 
     String json = mvcResult.getResponse().getContentAsString();
 
-    ChatMessage object = (ChatMessage)new ObjectMapper().readValue(json, ChatMessage.class);
+    ObjectMapper objectMapper =
+        new ObjectMapper().registerModule(new ParameterNamesModule()).registerModule(new Jdk8Module()).registerModule(new JavaTimeModule());
+
+    ChatMessage object = (ChatMessage)objectMapper.readValue(json, ChatMessage.class);
 
     assertThat(json).isNotNull();
   }
