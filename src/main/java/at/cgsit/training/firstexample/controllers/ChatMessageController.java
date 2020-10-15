e51010224caadba25382c2159b074df337c0a091
ext_christian.schaef <christian.schaefer@cgs.at>
2020-10-15 15:36:38 +0200

    # Chat message Controller

--- src/main/java/at/cgsit/training/firstexample/controllers/ChatMessageController.java
+++ src/main/java/at/cgsit/training/firstexample/controllers/ChatMessageController.java
@@ -18,30 +18,65 @@ import javax.validation.Valid;
 @Controller
 public class ChatMessageController {
 
-    private ChatMessageService chatMessageService;
+  private ChatMessageService chatMessageService;
 
-    private ChatMessageToChatMessageDTO cmToCMDTO;
+  private ChatMessageToChatMessageDTO cmToCMDTO;
 
-    @Autowired
-    public void setCmToCMDTO(ChatMessageToChatMessageDTO cmToCMDTO) {
-      this.cmToCMDTO = cmToCMDTO;
+  @Autowired
+  public void setCmToCMDTO(ChatMessageToChatMessageDTO cmToCMDTO) {
+    this.cmToCMDTO = cmToCMDTO;
+  }
+
+  @Autowired
+  public void setChatMessageService(ChatMessageService chatMessageService) {
+    this.chatMessageService = chatMessageService;
+  }
+
+  @RequestMapping("/")
+  public String redirToList(){
+    return "redirect:/chatmessage/list";
+  }
+
+  @RequestMapping({"/chatmessage/list", "/chatmessage"})
+  public String listChatMessages(Model model){
+    model.addAttribute("chatmessagelist", chatMessageService.listAll());
+    return "chatmessage/list";
+  }
+
+  @RequestMapping("/chatmessage/show/{id}")
+  public String getChatMessage(@PathVariable String id, Model model){
+    model.addAttribute("chatmessage", chatMessageService.getById(Long.valueOf(id)));
+    return "chatmessage/show";
+  }
+
+  @RequestMapping("chatmessage/edit/{id}")
+  public String editChatmessage(@PathVariable String id, Model model){
+    ChatMessage product = chatMessageService.getById(Long.valueOf(id));
+    ChatMessageDTO chatMessageDTO = cmToCMDTO.convert(product);
+
+    model.addAttribute("chatMessageform", chatMessageDTO);
+    return "chatmessage/chatmessageform";
+  }
+
+  @RequestMapping("/chatmessage/new")
+  public String newChatMessage(Model model){
+    model.addAttribute("chatMessageform", new ChatMessageDTO());
+    return "chatmessage/chatmessageform";
+  }
+
+  @RequestMapping(value = "/chatmessage", method = RequestMethod.POST)
+  public String saveOrUpdateChatMessage(@Valid @ModelAttribute("chatMessageform") ChatMessageDTO chatMessageDTO, BindingResult bindingResult){
+
+    if(bindingResult.hasErrors()){
+      return "chatmessage/chatmessageform";
     }
+    ChatMessage savedProduct = chatMessageService.saveOrUpdateChatMessageDTO(chatMessageDTO);
+    return "redirect:/chatmessage/show/" + savedProduct.getId();
+  }
 
-    @Autowired
-    public void setChatMessageService(ChatMessageService chatMessageService) {
-      this.chatMessageService = chatMessageService;
-    }
-
-    @RequestMapping("/")
-    public String redirToList(){
-      return "redirect:/chatmessage/list";
-    }
-
-    @RequestMapping({"/chatmessage/list", "/chatmessage"})
-    public String listChatMessages(Model model){
-      model.addAttribute("chatmessagelist", chatMessageService.listAll());
-      return "chatmessage/list";
-    }
-
-
+  @RequestMapping("/chatmessage/delete/{id}")
+  public String delete(@PathVariable String id){
+    chatMessageService.delete(Long.valueOf(id));
+    return "redirect:/chatmessage/list";
+  }
 }
